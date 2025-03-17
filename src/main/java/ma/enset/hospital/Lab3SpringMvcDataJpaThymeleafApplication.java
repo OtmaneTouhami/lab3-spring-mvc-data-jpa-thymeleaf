74547208b82f0589e42dcddd12078f89246cd395
OtmaneTouhami <otmanetouhami01@gmail.com>
2025-03-17 14:56:48 +0000

    First commit

---
+++ src/main/java/ma/enset/hospital/Lab3SpringMvcDataJpaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SpringMvcDataJpaThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3SpringMvcDataJpaThymeleafApplication.class, args);
+    }
+
+}
