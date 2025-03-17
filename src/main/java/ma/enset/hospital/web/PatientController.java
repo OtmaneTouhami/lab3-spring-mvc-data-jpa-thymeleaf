b823bf0b45fde934e5980a70827827cf4af5d1e3
OtmaneTouhami <otmanetouhami01@gmail.com>
2025-03-17 18:20:24 +0000

    add pagination and style

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -3,11 +3,12 @@ package ma.enset.hospital.web;
 import lombok.AllArgsConstructor;
 import ma.enset.hospital.entities.Patient;
 import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -15,9 +16,15 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping(path = "/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(
+            Model model,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "4") int size
+    ) {
+        Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patients", patientsPage.getContent());
+        model.addAttribute("pages", new int[patientsPage.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 }
