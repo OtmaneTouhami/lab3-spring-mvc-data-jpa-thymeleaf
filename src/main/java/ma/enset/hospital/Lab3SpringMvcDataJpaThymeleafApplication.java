7f46cace26cb8ee7756084ea43ce83bf51d2a16a
Otmane TOUHAMI <otmanetouhami01@gmail.com>
2025-04-12 22:30:54 +0100

    Add a Simple Authentification System

--- src/main/java/ma/enset/hospital/Lab3SpringMvcDataJpaThymeleafApplication.java
+++ src/main/java/ma/enset/hospital/Lab3SpringMvcDataJpaThymeleafApplication.java
@@ -5,6 +5,9 @@ import ma.enset.hospital.repository.PatientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -20,6 +23,11 @@ public class Lab3SpringMvcDataJpaThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab3SpringMvcDataJpaThymeleafApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     // @Bean
--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -6,6 +6,7 @@ import ma.enset.hospital.entities.Patient;
 import ma.enset.hospital.repository.PatientRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PatientController {
     private PatientRepository patientRepository;
 
-    @GetMapping(path = "/index")
+    @GetMapping(path = "/user/index")
     public String index(
             Model model,
             @RequestParam(name = "page", defaultValue = "0") int page,
@@ -34,24 +35,27 @@ public class PatientController {
         return "patients";
     }
 
-    @GetMapping(path = "/delete")
+    @GetMapping(path = "/admin/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String delete(Long id, String keyword, int page) {
         patientRepository.deleteById(id);
-        return "redirect:/index?page=" + page + "&keyword=" + keyword;
+        return "redirect:/user/index?page=" + page + "&keyword=" + keyword;
     }
 
     @GetMapping(path = "/")
     public String home() {
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping(path = "/patientForm")
+    @GetMapping(path = "/admin/patientForm")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String patientForm(Model model) {
         model.addAttribute("patient", new Patient());
         return "patientForm";
     }
 
-    @PostMapping(path = "/save")
+    @PostMapping(path = "/admin/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String save(
             Model model,
             @Valid Patient patient,
@@ -63,19 +67,30 @@ public class PatientController {
             return "patientForm";
         }
         patientRepository.save(patient);
-        return "redirect:/index?page=" + page + "&keyword=" + keyword;
+        return "redirect:/user/index?page=" + page + "&keyword=" + keyword;
     }
 
-    @GetMapping(path = "/edit/{id}")
+    @GetMapping(path = "/admin/edit/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String edit(Model model, @PathVariable("id") Long id, String keyword, int page) {
         Patient patient = patientRepository.findById(id).orElse(null);
         if (patient == null) {
-            return "redirect:/index";
+            return "redirect:/user/index";
         }
         model.addAttribute("patient", patient);
         model.addAttribute("keyword", keyword);
         model.addAttribute("page", page);
         return "patientUpdateForm";
+    }
+
+    @GetMapping(path = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(path = "/notAuthorized")
+    public String notAuthorized() {
+        return "notAuthorized";
     }
 
 }
